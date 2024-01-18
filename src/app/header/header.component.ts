111added730b4478ce57ddd54c3237b6d694f34d
rishulpu1 <rishulpu1@gmail.com>
2024-01-18 20:36:57 +0530

    working navigation with event emmiter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  @Output() loadedFeature = new EventEmitter<string>();
+  onSelect(feature: string) {
+    this.loadedFeature.emit(feature);
+  }
 }
